b07bd3b2b57301d93c5c995900fbd29f23ed5f42
rokios <rokios.playbasis@gmail.com>
2012-11-28 17:57:45 +0700

    check point -> add video

--- slide5.php
+++ slide5.php
@@ -12,7 +12,9 @@
         <div class="pbd_top_panel pbd_cover_panel">
 	        <div class="pbd_right_panel pbd_panel">
 	        	<div class='pbd_sample_context' id='pbd_context_visit'>
-					<div class="pbd_context_content"></div>
+					<div class="pbd_context_content">
+						<iframe width="360" height="203" src="http://www.youtube.com/embed/9bZkp7q19f0" frameborder="0" allowfullscreen=""></iframe>
+					</div>
 					<div class="pbd_context_action"></div>
 				</div>
 	        </div>
@@ -20,7 +22,7 @@
 
 	        <div class="pbd_left_panel pbd_panel">
 	        	
-	        	<h1 class="pbd_topic_header">Ecommerce Integration</h1>
+	        	<h1 class="pbd_topic_header">Video Content Integration</h1>
 	        	<div class="pbd_topic_content">
 	        		Lorem ipsum dolor sit amet, consectetur adipisicing elit,
 	        		 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -35,7 +37,7 @@
 	        
 			<div class="pbd_right_panel pbd_panel ">
 	        	
-	        	<h1 class="pbd_topic_header">Ecommerce Integration</h1>
+	        	<h1 class="pbd_topic_header">Comments Integration</h1>
 	        	<div class="pbd_topic_content">
 	        		Lorem ipsum dolor sit amet, consectetur adipisicing elit,
 	        		 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
